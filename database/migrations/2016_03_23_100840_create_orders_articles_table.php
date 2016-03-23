a658a4c16670081388acb8b694a1c44be75576b2
Petterromhagen <petter.romhagen@hotmail.com>
2016-03-23 11:14:42 +0100

    La till migrations

---
+++ database/migrations/2016_03_23_100840_create_orders_articles_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders_articles' function (Blueprint $table) {
+            $table->increments('order_id');
+            $table->string('article_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('orders_articles');
+    }
+}
