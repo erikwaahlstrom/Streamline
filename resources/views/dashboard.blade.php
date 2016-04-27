9491718eaecea1e044564f893b05022844cc3ccd
Petterromhagen <petter.romhagen@hotmail.com>
2016-04-27 16:06:33 +0200

    Welcome-text

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,14 +4,24 @@
 <div class="title">Välkommen till Heléns Rör</div>
 	<div class="welcome-box">
 	<div class="content-welcome">
-	<h5>Varmt välkommen till bokningssystemet för Heléns Rör.</h5>
-	<p>Om ni som leverantör vill boka en leveranstid finns valet att lägga till bokningar, och att visa de bokningar som ni redan har gjort, i sidmenyn.</p>
 
-	<p>Efter att en bokning av leverans har lagts till i systemet kommer ni att få en <b>referensnyckel</b> som skall medtas av transportören för att kunna registrera sin ankomst till lagret i Halmstad. </p>
+		<h5>Boka leverans</h5>
+			<p>Boka din leverans via menyn till vänster.</p>
 
-	<p>När transportören har registrerat sin ankomst på plats kommer de att få information om leveransen för att bekräfta att det är den leverans de har kommit med. Därefter visas information om var de ska köra för avlossning och vilken tid de får köra in.  </p>
+		<h5>Mina bokningar</h5>
+			<p>I mina bokningar får du en översikt över dina bokade leveranser.</p>
 
-	<p>Ifall ni har frågor eller funderingar, var goda och hänvisa till <a target="_blank" href="http://www.helens.se/kontakta-oss/direktkontakt.html">kontaktsidan</a> för kontaktinformation.</p>
+		<h5>Referensnyckel</h5>
+			<p>Referensnyckeln skall medtas av transportören och registreras vid ankomst
+			till lagret. Du får din referensnyckel efter du gjort din bokning.</p>
+
+		<h5>Transportörsinformation</h5>
+			<p>Vid ankomst till lagret registrerar transportören referensnyckeln, för att
+			matcha leveransen med bokningen. Därefter får transportören information om plats
+			och tid för avlossning.</p>
+
+		<h5>Kontaksida</h5>
+			<p>Vid frågor, klicka in på vår <a target="_blank" href="http://www.helens.se/kontakta-oss/direktkontakt.html">kontaktsidan</a>.</p>
 	</div>
 </div>
 @stop
