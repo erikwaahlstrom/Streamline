f92aa66d9adc33812de2c1d99a81919e05cf39e3
Christopher Wohlfarth <chris.wohlfarth@gmail.com>
2016-05-02 15:19:15 +0200

    Byta ut labels till placeholders i newbooking

--- public/js/main.js
+++ public/js/main.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
     $(document).on('click','.addorder',function(e){
        e.preventDefault();
        i++;
-       console.log(i); 
-       var input = '<div class="orderbox"><h6>Order ' + i + '</h6>' + '<div class="input-field">' + '<input id="order-' + i + '"' + 'type="text" class="validate" name="orders[' + i + '][ordernummer]">' + '<label for="order-' + i + '">' + 'Ordernummer</label>' + '</div>' + '</div>' + '<button class="btn waves-effect default #000000 addarticle">Lägg till artikel<i class="material-icons right">add</i></button>';
+       console.log(i);
+       var input = '<div class="orderbox"><h6>Order ' + i + '</h6>' + '<div class="input-field">' + '<input placeholder="Ordernummer" id="order-' + i + '"' + 'type="text" class="validate" name="orders[' + i + '][ordernummer]">' + '</div>' + '</div>' + '<button class="btn waves-effect default #000000 addarticle">Lägg till artikel<i class="material-icons right">add</i></button>';
     //    $('#bookingform').append(input);
         $(input).insertBefore(this);
 
@@ -23,17 +23,17 @@ $(document).ready(function(){
 
     // $(document).one('click','.addarticle',function(e){
     //     i--;
-    //     console.log(i); 
+    //     console.log(i);
     // });
 
-    $(document).on('click','.addarticle',function(e){ 
+    $(document).on('click','.addarticle',function(e){
         e.preventDefault();
         console.log(i);
-        var articleInput = '<div class="articlebox"><h6 class="article">Artikel ' + articleCount + '</h6>' + '<div class="input-field">' + '<input id="article-' + articleCount + '"' + 'type="text" class="validate" name="orders[' + i + '][articles][' + articleCount + ']">' + '<label for="article-' + articleCount + '">' + 'Artikelnummer</label>' + '</div>' + '<div class="input-field">' + '<input id="article-' + articleCount + '"' + 'type="text" class="validate" name="orders[' + i + '][articles][' + articleCount + '][weight]">' + '<label for="article-' + articleCount + '">' + 'Vikt</label>' + '</div>' + '<div class="input-field">' + '<input id="article-' + articleCount + '"' + 'type="text" class="validate" name="orders[' + i + '][articles][' + articleCount + '][lenght]">' + '<label for="article-' + articleCount + '">' + 'Längd</label>' + '</div>' + '<div class="input-field">' + '<input id="article-' + articleCount + '"' + 'type="text" class="validate" name="orders[' + i + '][articles][' + articleCount + '][position]">' + '<label for="article-' + articleCount + '">' + 'Position</label>' + '</div>' + '</div>';
+        var articleInput = '<div class="articlebox"><h6 class="article">Artikel ' + articleCount + '</h6>' + '<div class="input-field">' + '<input placeholder="Artikelnummer" id="article-' + articleCount + '"' + 'type="text" class="validate" name="orders[' + i + '][articles][' + articleCount + ']">' + '</div>' + '<div class="input-field">' + '<input placeholder="Vikt" id="article-' + articleCount + '"' + 'type="text" class="validate" name="orders[' + i + '][articles][' + articleCount + '][weight]">' + '</div>' + '<div class="input-field">' + '<input placeholder="Längd" id="article-' + articleCount + '"' + 'type="text" class="validate" name="orders[' + i + '][articles][' + articleCount + '][lenght]">' + '</div>' + '<div class="input-field">' + '<input placeholder="Position" id="article-' + articleCount + '"' + 'type="text" class="validate" name="orders[' + i + '][articles][' + articleCount + '][position]">' + '</div>' + '</div>';
         $(articleInput).insertBefore(this);
         articleCount++;
         console.log();
- 
+
         $(document).on('click','.addorder',function(e){
             articleCount = 1;
         });
@@ -83,7 +83,7 @@ $(document).ready(function(){
             email: {
                 required: true,
                 email: true
-            }, 
+            },
             password: {
                 required: true,
                 minlength: 5
@@ -93,7 +93,7 @@ $(document).ready(function(){
                 minlength: 5,
                 equalTo: "#password"
             },
-         },  
+         },
          messages: {
             email: {
                 required: 'Ange en email adress',
@@ -116,12 +116,12 @@ $(document).ready(function(){
             email: {
                 required: true,
                 email: true
-            }, 
+            },
             password: {
                 required: true,
                 minlength: 5
             },
-         },  
+         },
          messages: {
             email: {
                 required: 'Vänligen ange en email adress',
